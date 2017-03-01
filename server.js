34a90fd2efcc9d774387f62b4f7c6a66363bfce4
Alaoui <alsalah6@hotmail.com>
2017-03-01 21:54:28 +0100

    Time 26:00

--- server.js
+++ server.js
@@ -1,7 +1,37 @@
-let app = require('express')()
+let express = require('express')
+let app = express()
+let bodyParser = require('body-parser')
+let session = require('express-session')
 
+//Moteur de template
+app.set('view engine', 'ejs')
+
+//Middelware
+app.use('/assets', express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(session({
+  secret: 'nothingspecial',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false } //no https
+}))
+
+//Routes
 app.get('/', (request, response) => {
-  response.send("Salut !")
+  if(request.session.error){
+    response.locals.error = request.session.error
+    request.session.error = undefined
+
+  }
+  response.render("pages/index.ejs")
+})
+
+app.post('/', (request, response) => {
+  if (request.body.message === undefined || request.body.message === ''){
+    request.session.error = "Vous n'avez pas entré de message ! ";
+    response.redirect('/')
+  }
 })
 
 app.listen(8080)
